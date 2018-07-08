733dfc63523ecc80ee281eac7a1da327d5fe40a7
David Wang <davidwang@uber.com>
2018-07-07 19:55:02 -0700

    finished placing icons for ranking

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,12 @@ function readFromCSV() {
 
 }
 
+// If the user has saved a ranking via id then recover it here
+// returns: List of ranked trainees
+function getRanking() {
+
+}
+
 // Uses populated local data structure from readFromCSV to populate table
 function populateTable() {
   // Currently just duplicates the first table entry
@@ -13,4 +19,23 @@ function populateTable() {
   }
 }
 
+
+// Uses populated local data structure from getRanking to populate ranking
+function populateRanking() {
+  // Currently just duplicates first ranking entry
+  let rowNums = [1, 2, 4, 5]
+  let ranking = document.getElementById("ranking__pyramid");
+  let rankRows = Array.from(ranking.children).slice(1); // remove the title element
+  console.log(rankRows);
+  let rankEntry = rankRows[0].children[0];
+  console.log(rankEntry);
+  for (let i = 1; i < rowNums.length; i++) {
+    let rankRow = rankRows[i];
+    for (let j = 0; j < rowNums[i]; j++) {
+      rankRow.appendChild(rankEntry.cloneNode(true));
+    }
+  }
+}
+
 populateTable();
+populateRanking();
