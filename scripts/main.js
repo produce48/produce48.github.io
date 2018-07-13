70d996a58d74af94cea2ac55d603183ea47bd812
bourbonnais <csybourbonnais@gmail.com>
2018-07-13 16:12:44 -0700

    new csv column for ordering purposes

--- scripts/main.js
+++ scripts/main.js
@@ -78,7 +78,7 @@ function convertCSVArrayToTraineeData(csvArrays) {
     trainee.birthyear = traineeArray[5];
     trainee.eliminated = traineeArray[6] === 'e'; // sets trainee to be eliminated if 'e' appears in 6th col
     trainee.top12 = traineeArray[6] === 't'; // sets trainee to top 12 if 't' appears in 6th column
-    trainee.id = parseInt(traineeArray[7]); // trainee id is the original ordering of the trainees in the first csv
+    trainee.id = parseInt(traineeArray[7]-1); // trainee id is the original ordering of the trainees in the first csv
     trainee.image =
       trainee.name_romanized.replace(" ", "").replace("-", "") + ".jpg";
     return trainee;
